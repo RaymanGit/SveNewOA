2f69cf8e952c5fa635151132e98f5869e6ad9fbb
qq25625607 <25625607@qq.com>
2014-09-04 14:05:30 +0800

    添加学历学校信息管理功能

---
+++ Cn.Com.Sve.OA.Web/Controllers/QualificationSchoolController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Cn.Com.Sve.OA.BusinessService;
+using Cn.Com.Sve.OA.Entities;
+using Cn.Com.Sve.OA.Entities.Criteria;
+using Cn.Com.Sve.OA.Web.Models;
+using Cn.RaymanLee.Web.UI.ViewModels.LigerUI;
+using Cn.RaymanLee.Data;
+
+namespace Cn.Com.Sve.OA.Web.Controllers {
+	public partial class QualificationSchoolController : BaseController {
+		private object ToJson(QualificationSchool e) {
+			return new {
+				Id = e.Id,
+				Name = e.Name
+			};
+		}
+
+		partial void AddRowToGridModel(LigerGridModel m, QualificationSchool e) {
+			m.Rows.Add(this.ToJson(e));
+		}
+		partial void AfterAddData(QualificationSchool e, ref ActionResult ar) {
+			ar = this.Json(new AjaxOperationResult {
+				Data = this.ToJson(e),
+				Successful = true
+			});
+		}
+		partial void AfterUpdateData(QualificationSchool e, ref ActionResult ar) {
+			ar = this.Json(new AjaxOperationResult {
+				Data = this.ToJson(e),
+				Successful = true
+			});
+		}
+
+		public ActionResult GetInitData() {
+			return this.Json(new { }, JsonRequestBehavior.AllowGet);
+		}
+	}
+}
